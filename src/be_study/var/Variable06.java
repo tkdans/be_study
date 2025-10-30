b51af1cb0bcdee496a47a9cbeefb5d955e80b4d2
tkdans <psmoon81@naver.com>
2025-10-30 16:50:41 +0900

    변수 입출력 추가내용

--- src/be_study/var/Variable06.java
+++ src/be_study/var/Variable06.java
@@ -3,27 +3,27 @@ package be_study.var;
 import java.util.Scanner;
 
 public class Variable06 {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 문자열을 비교하는 equals ()메소드 사용
+		// 값이 같은가? 비교하는 연산자 ==
+		// ==비교 결과 --> 논리형 (true/ false)
 		Scanner scanner = new Scanner(System.in);
 		int num1 = scanner.nextInt();
 		int num2 = 10;
+		boolean b1 = (num1 == num2);// 결과가 논리값
+		System.out.println(b1);
 		
-		// 값이 같은가? 비교하는 연산자 ==
-		// ==비교 결과 --> 논리형 (true/ false)
-		boolean b1 = (num1 == num2);// 결과가 논리
-				System.out.println(b1);
-				scanner. nextLine(); //앞에 남은 엔터(\n) 청소
-				
-				String str1 = scanner.nextLine();
-				String str2 = "햄버거";
-				System.out.println(str1);
-				System.out.println(str2);
-				System.out.println(str1==str2);//숫자열 안된다. 
-				System.out.println(str1.equals(str2));
-				System.out.println(str2.equals(str1));
-				System.out.println(str2.equals("햄버거"));
+		scanner.nextLine(); // 앞에 남은 엔터(\n) 청소
+
+		String str1 = scanner.nextLine();// 입력값
+		String str2 = "햄버거";
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str1 == str2);// 숫자열 안된다.
+		System.out.println(str1.equals(str2));
+		System.out.println(str2.equals(str1));
+		System.out.println(str2.equals("햄버거"));
 	}
 
 }
