94666fb6fb7210456ad962384d5d8777a1c2e20a
tkdans <psmoon81@naver.com>
2025-11-05 10:16:40 +0900

    퀴즈11 반복문

---
+++ src/be_study/quiz/Quiz11.java
@@ -0,0 +1,32 @@
+package be_study.quiz;
+
+public class Quiz11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		for(int i = 1; i <= 5; i++) {
+//			System.out.print(i + "");
+//		}
+//		System.out.println();
+//		for(int i = 1; i <= 5; i++) {
+//			System.out.print(i + "");}
+//		System.out.println();
+//		for(int i = 1; i <= 5; i++) {
+//			System.out.print(i + "");}
+//		System.out.println();
+//		for(int i = 1; i <= 5; i++) {
+//			System.out.print(i + "");}
+//		System.out.println();
+//		for(int i = 1; i <= 5; i++) {
+//			System.out.print(i + "");}
+//		System.out.println();
+		System.out.print("1.");
+		for(int i =1; i<=5; i++) {
+			System.out.print(i);
+		}
+		System.out.println();
+			
+		
+
+	}
+	}
