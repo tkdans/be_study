a1ae998ceb3096c577878cb21e43d0dad8a043eb
tkdans <psmoon81@naver.com>
2025-11-12 14:06:54 +0900

    참조변수 배열복사, 향상된for문 enum(열고타입) \\\
    \\

---
+++ src/be_study/refer/Arr04.java
@@ -0,0 +1,10 @@
+package be_study.refer;
+
+public class Arr04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/be_study/refer/Arr05.java
@@ -0,0 +1,10 @@
+package be_study.refer;
+
+public class Arr05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
